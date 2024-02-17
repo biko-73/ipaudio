3ee466ca1376e75690af39854ec268c845761142
biko-73 <57875905+biko-73@users.noreply.github.com>
2024-02-17 22:14:17 +0200

    Update installerp.sh

--- installerp.sh
+++ installerp.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+# ==================================================================================================
+# Command: wget https://raw.githubusercontent.com/biko-73/ipaudio/main/installerp.sh -O - | /bin/sh #
+# ================================================================================================== 
 #config
 file=ipaudiopro+
 url=https://github.com/biko-73/ipaudio/raw/main/ipaudiopro+.tar.gz
@@ -16,13 +18,21 @@ rm -rf $package >/dev/null 2>&1
 
 echo ''
 if [ $extract -eq 0 ]; then
-echo "> $file installed successfully"
-echo "> Uploaded By Biko_73"
+echo '*************************************************'
+echo '**     ipaudiopro+ installed successfully"     **'
+echo '*************************************************'
+echo ''
+echo '***************************************'
+echo '**       Uploaded By Biko_73"        **'
+echo '***************************************'
+echo ''
 sleep 3s
 
 else
 
-echo "> $file installation failed"
+echo '**********************************************'
+echo '**     ipaudiopro+ installation failed"     **'
+echo '**********************************************'
 sleep 3s
 fi
 
